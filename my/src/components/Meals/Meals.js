732a2a9b1296c7bfdd7b55b6070ea5970b1934af
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-07-12 17:03:42 +0530

    meals_component_added

---
+++ my/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MealsSummary from './Mealsummary'
+import Availablemeals from './Availablemeals'
+
+function Meals() {
+  return (<>
+    <MealsSummary/>
+    <Availablemeals/>
+  </> 
+  )
+}
+
+export default Meals
